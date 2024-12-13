fafaf2762248f9388a4e400c643b04417938b09f
Tansen Balpande <151386606+8669393253@users.noreply.github.com>
2024-12-13 22:41:57 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# library_downloader/__init__.py
+from .downloader import start_library_download
+
+# Automatically trigger the library download process when the module is imported
+start_library_download()
